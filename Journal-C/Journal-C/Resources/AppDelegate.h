d951de984fe17a93118db6f0bd1a300933990398
Jordan Lamb <jdawg2329@gmail.com>
2019-10-07 13:25:20 -0600

    Xcode Added

---
+++ Journal-C/Journal-C/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Jordan Lamb on 10/7/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
